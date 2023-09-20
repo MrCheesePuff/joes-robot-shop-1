d8cda6eeee14508435702ec642894f6cd975ecc0
AlexM <31938690+MrCheesePuff@users.noreply.github.com>
2023-09-20 08:37:43 +0300

    added initial header & catalog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { InlineComponent } from './inline/inline.component';
+import { CatalogComponent } from './catalog/catalog.component';
+import { SiteHeaderComponent } from './site-header/site-header.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    InlineComponent
+    InlineComponent,
+    CatalogComponent,
+    SiteHeaderComponent
   ],
   imports: [
     BrowserModule
