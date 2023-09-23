119e6cf7fdeb07db558a32f45ce039d7bf0f5ccf
AlexM <31938690+MrCheesePuff@users.noreply.github.com>
2023-09-23 15:01:29 +0300

    added cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
@@ -15,6 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     CatalogComponent,
     SiteHeaderComponent,
     ProductDetailsComponent,
+    CartComponent
   ],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
